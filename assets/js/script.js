73fe0ddda93ff37074863487a4b40b3da791c054
JJackson510 <jamiej.jj5@gmail.com>
2021-12-10 22:26:40 -0600

    local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
 var pageContentEl = document.querySelector("#page-content");
+var task = [];
 
 var taskFormHandler = function(event) {
   event.preventDefault();
@@ -30,10 +31,13 @@ var taskFormHandler = function(event) {
   } else {
     var taskDataObj = {
       name: taskNameInput,
-      type: taskTypeInput
+      type: taskTypeInput,
+      status: "to do"
     };
 
     createTaskEl(taskDataObj);
+    console.log(taskDataObj);
+    console.log(taskDataObj.status)
   }
 };
 
@@ -48,9 +52,12 @@ var createTaskEl = function(taskDataObj) {
   listItemEl.appendChild(taskInfoEl);
 
   // create task actions (buttons and select) for task
-  var taskActionsEl = createTaskActions(taskIdCounter);
-  listItemEl.appendChild(taskActionsEl);
-  tasksToDoEl.appendChild(listItemEl);
+    var taskActionsEl = createTaskActions(taskIdCounter);
+    listItemEl.appendChild(taskActionsEl);
+    tasksToDoEl.appendChild(listItemEl);
+    
+    taskDataObj.id = taskIdCounter;
+    task.push(taskDataObj);
 
   // increase task counter for next unique id
   taskIdCounter++;
@@ -103,6 +110,14 @@ var completeEditTask = function(taskName, taskType, taskId) {
   taskSelected.querySelector("h3.task-name").textContent = taskName;
   taskSelected.querySelector("span.task-type").textContent = taskType;
 
+  //loop through task array and task object with new content
+    for (var i =0; i< task.length; i++) {
+    if (task[i].id === parseInt(taskId)) {
+        task[i].name = taskName;
+        task[i].type = taskType;
+    }
+};
+
   alert("Task Updated!");
 
   // remove data attribute from form
@@ -137,15 +152,23 @@ var taskStatusChangeHandler = function(event) {
   // convert value to lower case
   var statusValue = event.target.value.toLowerCase();
 
-  if (statusValue === "to do") {
+    if (statusValue === "to do") {
     tasksToDoEl.appendChild(taskSelected);
-  }
-   else if (statusValue === "in progress") {
+}
+    else if (statusValue === "in progress") {
     tasksInProgressEl.appendChild(taskSelected);
-  }
-   else if (statusValue === "completed") {
+}
+    else if (statusValue === "completed") {
     tasksCompletedEl.appendChild(taskSelected);
-  }
+}
+
+    //udate task's in task array
+    for (var i =0; i < task.length; i++) {
+        if (task[i].id === parseInt(taskId)) {
+            task[i].status = statusValue;
+        }
+    }
+    console.log(task);
 };
 
 var editTask = function(taskId) {
@@ -176,6 +199,18 @@ var deleteTask = function(taskId) {
   // find task list element with taskId value and remove it
   var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
   taskSelected.remove();
+
+    // create new array to hold updated list of tasks
+    var updatedTaskArr = [];
+    //loop through current tasks
+    for (var i= 0; i < task.length; i++) {
+        // if task[i].id doesn't match the value of taskID, ler's keep that task
+        if(task[i].id !== parseInt(taskId)) {
+            updatedTaskArr.push(task[i]);
+        }
+    }
+    //reassign task array to be the same as updatedTaskArr
+    task = updatedTaskArr;
 };
 
 // Create a new task
